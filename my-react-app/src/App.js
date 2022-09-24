6a80ae78844fb8094ab2e9efa6b4fdb343905233
JeongYoon-L <Jeongyoon.lee.1@stonybrook.edu>
2022-09-24 03:11:41 -0400

    Successfully using swipe in react

---
+++ my-react-app/src/App.js
@@ -0,0 +1,36 @@
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import './App.css';
+import "swiper/css"; //basic
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import ppap from './ppap.jpg'
+import thumb_guy from './thumb_guy.jpg'
+
+SwiperCore.use([Navigation, Pagination])
+
+function App() {
+  return (
+    <div className="App"> 
+      <Swiper
+        className="banner"
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>Slide 1
+          <img src = {ppap}></img>
+        </SwiperSlide>
+        <SwiperSlide>
+        <img src = {thumb_guy}></img>
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+  
+}
+
+export default App;
