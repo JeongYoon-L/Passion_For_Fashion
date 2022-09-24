40cbb6b3f62dbc796c27c202d38ed00554cd59aa
Mugen-Houyou <a4r2@daum.net>
2022-09-24 13:33:47 -0400

    이미지 조정 등

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,42 @@
 import { Swiper, SwiperSlide } from "swiper/react"; // basic
 import SwiperCore, { Navigation, Pagination } from "swiper";
-import './App.css';
 import "swiper/css"; //basic
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import ppap from './ppap.jpg'
-import thumb_guy from './thumb_guy.jpg'
+import styled from "styled-components";
+
+import './App.css';
+import ppap from './assets/ppap.jpg'
+import thumb_guy from './assets/thumb_guy.jpg'
+import thumb_dog_suit from './assets/126dfdd244674062d9b87a15855105fe.jpg';
+import thumb_dog_wobbling from './assets/633c9a7a98840f7cba6140488b4ad735.gif';
+import thumb_mumin from './assets/50b997ae70f654b97da961eb70016b78.png';
+
+
+const SSwiperSlide = styled(SwiperSlide)`
+  .swiper-container {
+    width: 100%;
+    margin: 40px 0;
+    padding: 45px 0;
+  }
+
+  .swiper-slide {
+    opacity: 0.4;
+    overflow: hidden;
+    transition: .7s;
+  }
+
+  .swiper-slide img {
+    width: 100%;
+  }
+
+  .swiper-slide-active {
+    opacity: 1;
+    z-index: 1;
+    transform: scale(1.5);
+}
+
+`;
 
 SwiperCore.use([Navigation, Pagination])
 
@@ -19,14 +50,21 @@ function App() {
         navigation
         pagination={{ clickable: true }}
       >
-        <SwiperSlide>Slide 1
-          <img src = {ppap}></img>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src = {thumb_guy}></img>
-        </SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        <SSwiperSlide>Slide 1
+          <img src={ppap}></img>
+        </SSwiperSlide>
+        <SSwiperSlide>
+          <img src={thumb_guy}></img>
+        </SSwiperSlide>
+        <SSwiperSlide>
+          <img src={thumb_dog_suit}></img>
+        </SSwiperSlide>
+        <SSwiperSlide>
+          <img src={thumb_dog_wobbling}></img>
+        </SSwiperSlide>
+        <SSwiperSlide>
+          <img src={thumb_mumin}></img>
+        </SSwiperSlide>
       </Swiper>
     </div>
   );
