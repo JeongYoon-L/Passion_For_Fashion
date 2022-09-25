69c553c5b8d4109b53a32521ae3387fbcf3f7cb2
Mugen-Houyou <a4r2@daum.net>
2022-09-24 21:35:15 -0400

    사진 등.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,11 +6,6 @@ import "swiper/css/pagination";
 import styled from "styled-components";
 
 import './App.css';
-import ppap from './assets/ppap.jpg'
-import thumb_guy from './assets/thumb_guy.jpg'
-import thumb_dog_suit from './assets/126dfdd244674062d9b87a15855105fe.jpg';
-import thumb_dog_wobbling from './assets/633c9a7a98840f7cba6140488b4ad735.gif';
-import thumb_mumin from './assets/50b997ae70f654b97da961eb70016b78.png';
 import DynamicSwiper from "./components/DynamicSwiper";
 
 
--- my-react-app/src/components/DynamicSwiper.js
+++ my-react-app/src/components/DynamicSwiper.js
@@ -13,24 +13,37 @@ import thumb_guy from '../assets/thumb_guy.jpg'
 import thumb_dog_suit from '../assets/126dfdd244674062d9b87a15855105fe.jpg';
 import thumb_dog_wobbling from '../assets/633c9a7a98840f7cba6140488b4ad735.gif';
 import thumb_mumin from '../assets/50b997ae70f654b97da961eb70016b78.png';
+import thumb_VIP from "../assets/VIP.jpg"
+import thumb_01 from "../assets/01.jpg"
+import thumb_02 from "../assets/02.jpg"
+import thumb_04 from "../assets/04.jpg"
+import thumb_05 from "../assets/05.jpg"
+import thumb_06 from "../assets/06.jpg"
+import thumb_07 from "../assets/07.jpg"
+import thumb_08 from "../assets/08.jpg"
+import thumb_09 from "../assets/09.jpg"
+import thumb_10 from "../assets/10.jpg"
+import thumb_11 from "../assets/11.jpg"
+
+import ImgPage from "./ImgPage";
 
 const DynamicSwiper = ()  => {
-  const [slides, setSlides] = useState(
-      [<SwiperSlide>
-        <img src={ppap}></img>
-      </SwiperSlide>,
+  const [slides, setSlides] = useState([
       <SwiperSlide>
-        <img src={ppap}></img>
+        <ImgPage imgUrl={thumb_11}> </ImgPage>
       </SwiperSlide>,
-      
-    ]
+
+      <SwiperSlide>
+        <ImgPage imgUrl={ppap}> </ImgPage>
+      </SwiperSlide>,
+      ]
     );
 
   const createNewSlide = () => {
     setSlides( prevSlides => [
         prevSlides,
         <SwiperSlide>
-          <img src={thumb_guy}></img>
+          <ImgPage imgUrl={thumb_mumin}> </ImgPage>
         </SwiperSlide>
       ]
     );
